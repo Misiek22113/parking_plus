7b306d32e1a3b18424f326dec895722e55c99101
Misiek22113 <97341476+Misiek22113@users.noreply.github.com>
2024-04-05 15:46:00 +0200

    fix overflow on dashboard

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -3,12 +3,12 @@ import ParkingSlotData from './ParkingSlotData';
 
 const Dashboard = () => {
   return (
-    <div className="h-full">
+    <div className="flex min-h-screen flex-col">
       <div className="flex flex-col gap-4 bg-[#374151] py-5">
         <ParkingSlotCount parkingSlotCount={10} isAvailableSlotCount={true} />
         <ParkingSlotCount parkingSlotCount={5} isAvailableSlotCount={false} />
       </div>
-      <div className="flex items-center justify-center bg-[#4B5563]">
+      <div className="flex flex-1 items-center justify-center bg-[#4B5563]">
         <ParkingSlotData />
       </div>
     </div>
