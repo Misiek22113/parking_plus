8a60a3cc2efddb2bb869c1fa84b89845945b49ca
Misiek22113 <97341476+Misiek22113@users.noreply.github.com>
2024-04-05 16:40:32 +0200

    small change

--- src/components/Dashboard/ParkingSlotData.tsx
+++ src/components/Dashboard/ParkingSlotData.tsx
@@ -1,10 +1,20 @@
 import Image from 'next/image';
 import PARKING_ICON from '../../assets/icons/square-parking.svg';
+import { useContext } from 'react';
+import { SelectedSlotContext } from '@/context/SelectedSlotContext';
 
 const ParkingSlotData = () => {
+  const { selectedSlotNumber } = useContext(SelectedSlotContext);
+
   return (
     <div className="h-full">
-      <Image src={PARKING_ICON} alt="Parking Icon" />
+      {selectedSlotNumber !== 0 ? (
+        <div className="grid grid-cols-5">
+          <div className="col-span-2">{selectedSlotNumber}</div>
+        </div>
+      ) : (
+        <Image src={PARKING_ICON} alt="Parking Icon" />
+      )}
     </div>
   );
 };
