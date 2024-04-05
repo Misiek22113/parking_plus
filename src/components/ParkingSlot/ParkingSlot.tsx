7edf0bf81923d22297917115658328532f922706
Misiek22113 <97341476+Misiek22113@users.noreply.github.com>
2024-04-05 16:24:01 +0200

    add selected slot context

--- src/components/ParkingSlot/ParkingSlot.tsx
+++ src/components/ParkingSlot/ParkingSlot.tsx
@@ -1,12 +1,18 @@
+'use client';
 export type ParkingSlotProps = {
   isAvailable: boolean;
   slotNumber: number;
 };
 
 const ParkingSlot = ({ isAvailable, slotNumber }: ParkingSlotProps) => {
+  const setIsSelected = () => {
+    console.log('Slot number:', slotNumber);
+  };
+
   return (
     <div
       className={`flex min-h-28 max-w-20 items-center justify-center rounded-lg text-5xl ${isAvailable ? ' bg-teal-500' : 'bg-red-500'}`}
+      onClick={setIsSelected}
     >
       {slotNumber}
     </div>
