06c0488febbc17d3e22a0307e28e9bdd29a15e3d
Misiek22113 <97341476+Misiek22113@users.noreply.github.com>
2024-04-17 20:15:02 +0200

    create subgroups

---
+++ src/app/(admin)/layout.tsx
@@ -0,0 +1,38 @@
+'use client';
+import { Inter } from 'next/font/google';
+import '../globals.css';
+import Dashboard from '@/components/Dashboard/Dashboard';
+import { SelectedSlotContext } from '@/context/SelectedSlotContext';
+import { useState } from 'react';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function UserLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const [selectedSlot, setSelectedSlot] = useState(0);
+
+  return (
+    <html lang="en">
+      <body className={`${inter.className}`}>
+        <SelectedSlotContext.Provider
+          value={{
+            selectedSlotNumber: selectedSlot,
+            setSelectedSlotNumber: setSelectedSlot,
+          }}
+        >
+          <div className="grid h-full grid-cols-12">
+            <div className="col-span-9 h-full items-center justify-items-center">
+              {children}
+            </div>
+            <div className="col-span-3 col-start-10">
+              <Dashboard />
+            </div>
+          </div>
+        </SelectedSlotContext.Provider>
+      </body>
+    </html>
+  );
+}
